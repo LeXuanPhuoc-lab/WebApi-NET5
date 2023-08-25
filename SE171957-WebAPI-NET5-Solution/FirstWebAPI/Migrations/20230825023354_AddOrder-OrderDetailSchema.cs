903fce75833930c5417c334208f2b3da65ffcdd2
Le Xuan Phuoc <stevenmarks147@gmail.com>
2023-08-25 10:31:50 +0700

    Demo Fluent API in EF Core

---
+++ SE171957-WebAPI-NET5-Solution/FirstWebAPI/Migrations/20230825023354_AddOrder-OrderDetailSchema.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FirstWebAPI.Migrations
+{
+    public partial class AddOrderOrderDetailSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_Category_CategoryId",
+                table: "Product");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Product_CategoryId",
+                table: "Product");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryEntityCategoryId",
+                table: "Product",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getutcdate()"),
+                    DeliveryDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    TotalPrice = table.Column<double>(type: "float", nullable: false),
+                    TotalQuantity = table.Column<int>(type: "int", nullable: false),
+                    Voucher = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    TotalQuantity = table.Column<int>(type: "int", nullable: false),
+                    TotalPrice = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => new { x.OrderId, x.ProductId });
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_CategoryEntityCategoryId",
+                table: "Product",
+                column: "CategoryEntityCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_ProductId",
+                table: "OrderDetails",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_Category_CategoryEntityCategoryId",
+                table: "Product",
+                column: "CategoryEntityCategoryId",
+                principalTable: "Category",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_Category_CategoryEntityCategoryId",
+                table: "Product");
+
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Product_CategoryEntityCategoryId",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryEntityCategoryId",
+                table: "Product");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_CategoryId",
+                table: "Product",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_Category_CategoryId",
+                table: "Product",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
